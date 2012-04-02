91608f3b4a15aa6f12fc8a1a1d24ae601a235805
Gavin Brock <gavin@brock-family.org>
2012-04-02 18:38:08 +0900

    Added checking of superdomain

--- loc.pl
+++ loc.pl
@@ -25,19 +25,23 @@ if ($router =~ /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[
   @hostnames = ($router);
 }
 
-#TODO Chop off subdomains - sometimes they have loc, but host doesn't
-# e.g. ae-62-62.csw1.Dallas1.Level3.net -> Dallas1.Level3.net 
-
-foreach my $host (@hostnames) {
-  if (my $packet = $res->query($host, 'LOC')) {
-    foreach my $answer ($packet->answer) {
-      if ($answer->isa('Net::DNS::RR::LOC')) {
-        print "$router => $host\n";
-        print "Long ".$answer->longitude."\n";
-        print "Lat ".$answer->latitude."\n";
+while (@hostnames) {
+  foreach my $host (@hostnames) {
+    if (my $packet = $res->query($host, 'LOC')) {
+      foreach my $answer ($packet->answer) {
+        if ($answer->isa('Net::DNS::RR::LOC')) {
+          my ($lat, $lon) = $answer->latlon;
+          print "$router => $host\n";
+          print "LL $lat,$lon\n";
+          exit;
+        }
       }
+    } else {
+      warn "No loc for $host";
     }
-  } else {
-    warn "No loc for $host";
   }
+
+  #Chop off subdomains - sometimes they have loc, but host doesn't
+  # e.g. ae-62-62.csw1.Dallas1.Level3.net -> Dallas1.Level3.net 
+  @hostnames = grep {$_} map { s/^[^.]+\.// && $_ } @hostnames;
 }
